0263d75aa856154e4e0b3ae359d9e1573df8213b
Juan David Garcia <juancho637@gmail.com>
2022-08-01 22:40:51 -0500

    añadiendo ejemplo de RTK Query

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,16 @@ import { Provider } from "react-redux";
 
 import { store } from "./store";
 // import App from "./App";
-import { PokemonApp } from "./PokemonApp";
+// import { PokemonApp } from "./PokemonApp";
+import { TodoApp } from "./TodoApp";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <App /> */}
-      <PokemonApp />
+      {/* <PokemonApp /> */}
+      <TodoApp />
     </Provider>
   </React.StrictMode>
 );
